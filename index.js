dcafbb84f727cd347bdedcf798b9138ef138e7e1
Garrett <garres4@vt.edu>
2021-10-14 11:34:09 -0600

    commit tree 6882b077bf7409b05e18cde5d7ef75f82a8230cc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
 
+  function winFinder(object) {
+      return object.result === "W";
+  }
+
+  function superbowlWin(array) {
+      let win = array.find(winFinder)
+      if (win) {
+          return win.year;
+      } else {
+         return win;
+      }
+  }
